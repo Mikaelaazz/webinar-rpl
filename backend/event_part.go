c4ef9246e7df04ac7aa1a39d1282afebf0a6aca5
Fernando Perry <fernandoperry1234@gmail.com>
2025-04-05 22:47:55 +0700

    small table change on event participant

--- backend/event_part.go
+++ backend/event_part.go
@@ -11,6 +11,7 @@ type EventParticipant struct {
     UserId       int    `gorm:"column:user_id"`
     EventPCome   bool   `gorm:"column:eventp_come"`
     EventPCert   string `gorm:"column:eventp_cert"`
+    EventPCode   string `gorm:"column:eventp_code"`
 
     Event        Event  `gorm:"foreignKey:EventId"`
     User         User   `gorm:"foreignKey:UserId"`
