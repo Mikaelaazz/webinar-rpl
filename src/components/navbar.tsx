c297f168ac08a61fdab96874a123044d24d34210
Mikaela <johaneswijaya341@gmail.com>
2025-04-29 09:08:25 +0700

    small update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,9 @@
-import { Button } from "@heroui/button";
-import { Kbd } from "@heroui/kbd";
 import { Link } from "@heroui/link";
 import { Input } from "@heroui/input";
+import { siteConfig } from "@/config/site";
+import { SearchIcon } from "@/components/icons";
+import { Logo } from "@/components/icons";
+import { ThemeSwitch } from "@/components/theme-switch";
 import {
   Navbar as HeroUINavbar,
   NavbarBrand,
@@ -11,44 +13,19 @@ import {
   NavbarMenu,
   NavbarMenuItem,
 } from "@heroui/navbar";
-import { link as linkStyles } from "@heroui/theme";
-import clsx from "clsx";
-
-import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
-import {
-  TwitterIcon,
-  GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
-} from "@/components/icons";
-import { Logo } from "@/components/icons";
-
-import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
-import { SearchIcon } from "@/components/icons";
-import { Logo } from "@/components/icons";
-
-// Navbar
 
 export const Navbar = () => {
   const searchInput = (
     <Input
       aria-label="Search"
       classNames={{
-        inputWrapper: "bg-default-100",
+        inputWrapper: "bg-default-200",
         input: "text-sm",
       }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
       labelPlacement="outside"
       placeholder="Search..."
       startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+        <SearchIcon className="text-base text-default-500 pointer-events-none flex-shrink-0" />
       }
       type="search"
     />
@@ -56,7 +33,7 @@ export const Navbar = () => {
 
   return (
     <HeroUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="flex sm:hidden basis-1 pl-4" justify="start">
+      <NavbarContent className="flex lg:hidden basis-1 pl-4" justify="start">
         <NavbarMenuToggle />
       </NavbarContent>
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -66,50 +43,28 @@ export const Navbar = () => {
             color="foreground"
             href="/"
           >
-            {/* <Logo /> */}
+            <Logo />
             <p className="font-bold text-inherit">Webinar UKDC</p>
           </Link>
         </NavbarBrand>
-        {/* <div className="hidden lg:flex gap-4 justify-start ml-2">
-
-        <div className="hidden sm:flex gap-4 justify-start ml-2">
-
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <Link
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium"
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </Link>
-            </NavbarItem>
-          ))}
-        </div> */}
-        </div>
+        <NavbarItem className="hidden lg:flex ml-3">
+          <Link href="/" color="foreground">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem className="hidden lg:flex">
+          <Link href="/about" color="foreground">
+            About
+          </Link>
+        </NavbarItem>
       </NavbarContent>
 
       <NavbarContent
         className="hidden sm:flex basis-1/5 sm:basis-full"
         justify="end"
       >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <ThemeSwitch />
-        </NavbarItem>
+        <ThemeSwitch />
         <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
-        <NavbarMenuToggle />
-      <NavbarContent className="flex sm:hidden basis-1 pl-4" justify="end">
-        <ThemeSwitch />
       </NavbarContent>
 
       <NavbarMenu>
