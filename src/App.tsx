330a8cbfd1a700d5ebffd8e43f93614a0582806b
Mikaela <johaneswijaya341@gmail.com>
2025-05-01 14:42:34 +0700

    added webinar & sertifikat admin

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,20 @@ import BlogPage from "@/pages/blog";
 import AboutPage from "@/pages/about";
 import LoginPage from "@/pages/auth/login";
 import RegisterPage from "@/pages/auth/register";
+// USER
 import DashboardPage from "@/pages/dashboard";
 import DetailPage from "@/pages/detail";
 import ProfilPage from "@/pages/profil";
+import SertifikatUserPage from "./pages/sertifikat";
+// ADMIN
+import DasboardAdminPage from "@/pages/admin/index";
+import ManageUserPage from "@/pages/admin/manageuser";
+import WebinarPage from "@/pages/admin/webinar";
+import DetailAdminPage from "@/pages/admin/detail";
+import SertifikatAdminPage from "@/pages/admin/serfitikat"; 
+import CreateSertifikatAdminPage from "@/pages/admin/add_sertifikat";
+
+
 
 function App() {
   return (
@@ -21,9 +32,19 @@ function App() {
       <Route element={<AboutPage />} path="/about" />
       <Route element={<LoginPage />} path="/login" />
       <Route element={<RegisterPage />} path="/register" />
+      {/* User */}
       <Route element={<DashboardPage />} path="/dashboard" />
       <Route element={<DetailPage />} path="/detail" />
       <Route element={<ProfilPage />} path="/profil" />
+      <Route element={<SertifikatUserPage />} path="/sertifikat" />
+      {/* Admin */}
+      <Route element={<DasboardAdminPage />} path="/admin" />
+      <Route element={<ManageUserPage />} path="/admin/user" />
+      <Route element={<WebinarPage />} path="/admin/webinar" />
+      <Route element={<DetailAdminPage />} path="/admin/detail" />
+      <Route element={<WebinarPage />} path="/admin/webinar/create" />
+      <Route element={<SertifikatAdminPage />} path="/admin/sertifikat" />
+      <Route element={<CreateSertifikatAdminPage />} path="/admin/sertifikat/create" />
     </Routes>
   );
 }
