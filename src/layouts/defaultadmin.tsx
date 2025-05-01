4587a0cfb19b813b881f63083dbddcec38051151
Mikaela <johaneswijaya341@gmail.com>
2025-05-01 14:40:42 +0700

    Create defaultadmin.tsx

---
+++ src/layouts/defaultadmin.tsx
@@ -0,0 +1,20 @@
+import { Link } from "@heroui/link";
+
+import { NavbarAdmin } from "@/components/navbaradmin";
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex flex-col h-screen">
+      <NavbarAdmin />
+      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
+        {children}
+      </main>
+      <footer className="w-full flex items-center justify-center py-3">
+      </footer>
+    </div>
+  );
+}
